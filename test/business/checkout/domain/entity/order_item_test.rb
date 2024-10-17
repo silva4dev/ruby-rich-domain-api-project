ce37a03fcad730be0279918f3d720dc83c37d1bd
silva4dev <web4dev.lucas@outlook.com>
2024-10-17 00:21:33 -0300

    test: :test_tube: add order item entity

---
+++ test/business/checkout/domain/entity/order_item_test.rb
@@ -0,0 +1,35 @@
+# typed: true
+# frozen_string_literal: true
+
+require_relative '../../../../test_helper'
+require_relative '../../../../../src/business/checkout/domain/entity/order_item'
+
+class OrderItemTest < Minitest::Test
+  def setup
+    @id_item = 1
+    @price = 50.0
+    @quantity = 2
+    @order_item = Checkout::Domain::Entity::OrderItem.new(@id_item, @price, @quantity)
+  end
+
+  def test_initialize
+    assert_equal @id_item, @order_item.id_item
+    assert_equal @price, @order_item.price
+    assert_equal @quantity, @order_item.quantity
+  end
+
+  def test_initialize_with_invalid_quantity
+    assert_raises(ArgumentError) do
+      Checkout::Domain::Entity::OrderItem.new(@id_item, @price, 0)
+    end
+
+    assert_raises(ArgumentError) do
+      Checkout::Domain::Entity::OrderItem.new(@id_item, @price, -1)
+    end
+  end
+
+  def test_get_total
+    expected_total = @price * @quantity
+    assert_equal expected_total, @order_item.get_total
+  end
+end
