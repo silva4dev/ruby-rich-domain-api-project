628ed1b8b7ae578ea2ce111764c55ba7947bc248
silva4dev <web4dev.lucas@outlook.com>
2024-10-18 20:55:37 -0300

    test: :test_tube: add value object to cpf

---
+++ test/core/value_object/cpf_test.rb
@@ -0,0 +1,18 @@
+# typed: true
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+require_relative '../../../src/core/value_object/cpf'
+
+class CpfTest < Minitest::Test
+  def test_cpf_initialization
+    @cpf = ValueObject::Cpf.new('17724117012')
+    assert @cpf, '177.241.170-12'
+  end
+
+  def test_cpf_initialization_with_invalid
+    assert_raises(ArgumentError, 'Cpf Inválido') do
+      ValueObject::Cpf.new('123456789')
+    end
+  end
+end
