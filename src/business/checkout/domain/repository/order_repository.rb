148bf3c0ebe095497ab1596907da90c20f8d086d
silva4dev <web4dev.lucas@outlook.com>
2024-10-19 19:28:53 -0300

    feat: ✨ add abstract class to order repository

--- src/business/checkout/domain/repository/order_repository.rb
+++ src/business/checkout/domain/repository/order_repository.rb
@@ -13,6 +13,21 @@ module Checkout
         def save(order)
           raise NotImplementedError
         end
+
+        sig { returns(Integer) }
+        def count
+          raise NotImplementedError
+        end
+
+        sig { void }
+        def clean
+          raise NotImplementedError
+        end
+
+        sig { params(code: String).returns(Entity::Order) }
+        def get(code)
+          raise NotImplementedError
+        end
       end
     end
   end
