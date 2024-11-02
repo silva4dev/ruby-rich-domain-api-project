ffdb3de8058c269d96243ad012b2e8d96e1b56b4
silva4dev <web4dev.lucas@outlook.com>
2024-11-02 02:25:16 -0300

    feat: :sparkles: add connection

---
+++ src/business/checkout/infrastructure/database/connection.rb
@@ -0,0 +1,20 @@
+# typed: true
+# frozen_string_literal: true
+
+require 'sorbet-runtime'
+
+module Checkout
+  module Infrastructure
+    module Database
+      class Connection
+        extend T::Sig
+
+        sig { params(statement: String, params: T.untyped).returns(T.untyped) }
+        def query(statement, params); end
+
+        sig { returns(NilClass) }
+        def close; end
+      end
+    end
+  end
+end
