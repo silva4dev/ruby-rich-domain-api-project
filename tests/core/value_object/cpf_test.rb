ef0bbf80c47fd1098fd1ef9cc2bf2c18d0f5c03d
silva4dev <web4dev.lucas@outlook.com>
2024-11-21 22:46:43 -0300

    refactor: :sparkles: add module to class cpf

--- tests/core/value_object/cpf_test.rb
+++ tests/core/value_object/cpf_test.rb
@@ -4,16 +4,20 @@
 require_relative '../../test_helper'
 require_relative '../../../src/core/value_object/cpf'
 
-class CpfTest < Minitest::Test
-  def test_cpf_initialization
-    @cpf = ValueObject::Cpf.new('17724117012')
+module Core
+  module ValueObject
+    class CpfTest < Minitest::Test
+      def test_cpf_initialization
+        @cpf = Cpf.new('17724117012')
 
-    assert @cpf, '177.241.170-12'
-  end
+        assert @cpf, '177.241.170-12'
+      end
 
-  def test_cpf_initialization_with_invalid
-    assert_raises(ArgumentError, 'Cpf Inválido') do
-      ValueObject::Cpf.new('123456789')
+      def test_cpf_initialization_with_invalid
+        assert_raises(ArgumentError, 'Cpf Inválido') do
+          Cpf.new('123456789')
+        end
+      end
     end
   end
 end
