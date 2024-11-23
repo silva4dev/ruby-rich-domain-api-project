11207e28b7570385beb77363b46b010038ab9f55
silva4dev <web4dev.lucas@outlook.com>
2024-11-23 13:33:17 -0300

    feat: :sparkles: add http adapter

---
+++ src/business/freight/infrastructure/http/http.rb
@@ -0,0 +1,22 @@
+# typed: true
+# frozen_string_literal: true
+
+module Freight
+  module Infrastructure
+    module Http
+      class HttpAdapter
+        extend T::Sig
+
+        sig { params(method: String, url: String, callback: T.proc.params(params: T.untyped, body: T.untyped).returns(T.untyped)).void }
+        def on(method, url, callback)
+          raise NotImplementedError
+        end
+
+        sig { params(port: Integer).void }
+        def listen(port)
+          raise NotImplementedError
+        end
+      end
+    end
+  end
+end
